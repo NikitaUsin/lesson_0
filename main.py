38ddc314022b8d0bc779839a654b6ce28f6e66c1
NikitaUsin <usinnikita19@gmail.com>
2025-01-11 11:45:20 +0300

    Add files via upload
    2 задание (Практическое задание по уроку "Базовые структуры данных")

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-
-
-print("Hello world!")
+print('Hello, World!')
+# 1st program
+print(9 ** 0.5 * 5)
+# "2st program"
+print(99.99 > 99.98 and 1000 != 1000.1)
+# "3st program"
+print(2*2+2)
+print(2*(2+2))
+print(6 == 8)
+# "4st program"
+print(float(123.456))
+print(123.456 * 10)
+print(int(123.456))
+print(1234.56 % 10)
